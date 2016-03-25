6385d28a0a9254b489f746af604f6f3dc7caff8c
mdpaustin <mphippeny@yahoo.com>
2016-03-25 16:31:29 -0500

    Added files via upload

--- lemonadeStand.py
+++ lemonadeStand.py
@@ -4,6 +4,16 @@ init()
 
 player_pantry = {'lemon_cups': 0, 'sugar_cups': 0, 'cups_cups': 0, 'cash': 15.00, 'savings': 0}
 
+def lem_title():
+    print(Style.BRIGHT + Fore.YELLOW)
+    print(" __                                                      __          __")
+    print("/\\ \\                                                    /\\ \\        /\\ \\")
+    print("\\ \\ \\         __    ___ ___     ___     ___      __     \\_\\ \\     __\\ \\ \\")
+    print(" \\ \\ \\  __  /'__`\\/' __` __`\\  / __`\\ /' _ `\\  /'__`\\   /'_` \\  /'__`\\ \\ \\")
+    print("  \\ \\ \\L\\ \\/\\  __//\\ \\/\\ \\/\\ \\/\\ \\L\\ \\/\\ \\/\\ \\/\\ \\L\\.\\_/\\ \\L\\ \\/\\  __/\\ \\_\\")
+    print("   \\ \\____/\\ \\____\\ \\_\\ \\_\\ \\_\\ \\____/\\ \\_\\ \\_\\ \\__/.\\_\\ \\___,_\\ \\____\\\\/\\_\\")
+    print("    \\/___/  \\/____/\\/_/\\/_/\\/_/\\/___/  \\/_/\\/_/\\/__/\\/_/\\/__,_ /\\/____/ \\/_/")
+    print(Style.RESET_ALL)
 def inventory():
     print("\nYou have:")
     if player_pantry['lemon_cups'] == 1:
@@ -21,6 +31,7 @@ def inventory():
     print(Fore.GREEN + "%.2f in cash" % player_pantry['cash'])
     print("%.2f in savings" % player_pantry['savings'])
 
+lem_title()
 inventory()
 '''while player_state != 'exit':  #the game loop
 	if player_state = 'exit':
